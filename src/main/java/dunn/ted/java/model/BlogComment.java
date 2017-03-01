5bc8524153bf849244a50f5b0d7726dd5fb174e4
Ted Dunn <tldunn451@gmail.com>
2017-03-01 17:45:47 -0500

    Add BlogComment model

--- src/main/java/dunn/ted/java/model/BlogComment.java
+++ src/main/java/dunn/ted/java/model/BlogComment.java
@@ -1,7 +1,30 @@
 package dunn.ted.java.model;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by Ted on 3/1/2017.
  */
 public class BlogComment {
+    private String author;
+    private LocalDateTime dateTime;
+    private String comment;
+
+    public BlogComment(String author, String comment) {
+        this.author = author;
+        this.comment = comment;
+        dateTime = LocalDateTime.now();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getComment() {
+        return comment;
+    }
 }
