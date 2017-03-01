8b165099f2833c863a2a4a95326d2eba1948c121
Ted Dunn <tldunn451@gmail.com>
2017-03-01 17:18:48 -0500

    Add BlogEntry model

---
+++ src/main/java/dunn/ted/java/model/BlogComment.java
@@ -0,0 +1,7 @@
+package dunn.ted.java.model;
+
+/**
+ * Created by Ted on 3/1/2017.
+ */
+public class BlogComment {
+}
---
+++ src/main/java/dunn/ted/java/model/BlogEntry.java
@@ -0,0 +1,38 @@
+package dunn.ted.java.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ted on 3/1/2017.
+ */
+public class BlogEntry {
+    private String title;
+    private LocalDateTime dateTime;
+    private String entry;
+    private List<BlogComment> comments;
+
+    public BlogEntry(String title, String entry) {
+        this.title = title;
+        this.entry = entry;
+        dateTime = LocalDateTime.now();
+        comments = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public List<BlogComment> getComments() {
+        return comments;
+    }
+}
